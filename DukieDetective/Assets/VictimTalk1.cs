ccb0bc32158619395545312b9bb82a38faec7aa2
GraicDaBoss <grace1gallagher@icloud.com>
2024-05-08 15:20:29 +0100

    New interactions

---
+++ DukieDetective/Assets/VictimTalk1.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using TMPro;
+
+public class VictimTalk1 : MonoBehaviour
+{
+    public UnityEvent enteredTrigger, exitedTrigger;
+    private bool isInsideTrigger;
+    public GameObject TalkPanel, PlayerCapsule, Camera;
+    public TMP_Text TalkText;
+    private string[] dialogues = { "", "Hey, are you a detective? You sure look like one..", "Listen, I have an important job for you", "On Thursday the 12th of June, I was MURDERED.." };
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        TalkPanel.SetActive(false);
+        Camera.SetActive(false);
+        PlayerCapsule.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isInsideTrigger && Input.GetKeyDown(KeyCode.E))
+        {
+            StartDialogue();
+            StartCoroutine(DisplayDialogues());
+        }
+    }
+
+    IEnumerator DisplayDialogues()
+    {
+        for (int i = 0; i < dialogues.Length; i++)
+        {
+            Debug.Log("Displaying dialogue: " + dialogues[i]); // Debug log to check the displayed dialogue
+            TalkText.text = dialogues[i];
+            yield return new WaitUntil(() => Input.GetKeyDown(KeyCode.E)); // Wait for the player to press E before displaying the next dialogue
+            yield return null; // Ensure we only proceed after the E key is released
+        }
+
+        EndDialogue();
+    }
+
+
+    void EndDialogue()
+    {
+        TalkPanel.SetActive(false);
+        Camera.SetActive(false);
+        PlayerCapsule.SetActive(true);
+    }
+
+    void StartDialogue()
+    {
+        TalkPanel.SetActive(true);
+        Camera.SetActive(true);
+        PlayerCapsule.SetActive(false);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            enteredTrigger.Invoke();
+            isInsideTrigger = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            exitedTrigger.Invoke();
+            isInsideTrigger = false;
+        }
+    }
+}
